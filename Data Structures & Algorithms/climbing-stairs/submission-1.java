1710dd058a97ff83cbde73220e193b8caa33dc2b
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-04-08 11:57:53 +1000

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public int climbStairs(int n) {
+        Map<Integer,Integer> cache = new HashMap<>();
+
+        return dfs(n, cache);
+    }
+
+    public int dfs(int n, Map<Integer,Integer> cache) {
+        int res = 0;
+
+        if (n > 1) {
+            // take one step
+            if (cache.containsKey(n - 1)) {
+                res += cache.get(n - 1);
+            }
+            else {
+                int tmp = dfs(n - 1, cache);
+                cache.put(n - 1, tmp);
+                res += tmp;
+            }
+
+            // take two steps 
+            if (cache.containsKey(n - 2)) {
+                res += cache.get(n - 2);
+            }
+            else {
+                int tmp = dfs(n - 2, cache);
+                cache.put(n - 2, tmp);
+                res += tmp;
+            }
+
+        }
+        else {
+            res = 1;  // if n = 1 or n = 0 result is the same.
+        }
+
+        return res;
+    }
+}
