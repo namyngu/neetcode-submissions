1c2bed5e2ba9df947a87e28a7473ca0a9cdaa3b1
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-04-20 23:21:52 +1000

    Add: quickSort - submission-2

---
+++ Data Structures & Algorithms/quickSort/submission-2.java
@@ -0,0 +1,57 @@
+// Definition for a pair.
+// class Pair {
+//     int key;
+//     String value;
+//
+//     public Pair(int key, String value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    public List<Pair> quickSort(List<Pair> pairs) {
+        if (pairs.size() <= 1) {
+            return pairs;
+        }
+
+        // sort the list
+        quickSortHelper(pairs, 0, pairs.size() - 1, pairs.size() - 1);
+
+        return pairs;
+    }
+
+    private void quickSortHelper(List<Pair> pairs, int l, int r, int pIndex) {
+        if (r - l + 1 <= 1) {
+            return;
+        }
+
+        Pair pivot = pairs.get(pIndex);
+        int swap = l;
+        int curr = l;
+
+        while (curr <= r) {
+            if (curr == pIndex) {
+                // if curr pointer is at the pivot value, skip.
+                curr++;
+                continue;
+            }
+
+            if (pairs.get(curr).key < pivot.key) {      // elements < pivot go to left and >= go to right because of specific requirements in the question.
+                Pair tmp = pairs.get(swap);
+                pairs.set(swap, pairs.get(curr));
+                pairs.set(curr, tmp);
+
+                swap++;
+            }
+
+            curr++;
+        }
+
+        // Swap pivot with value at swap ptr.
+        pairs.set(pIndex, pairs.get(swap));
+        pairs.set(swap, pivot);
+
+        quickSortHelper(pairs, l, swap - 1, swap - 1);   // sort left side
+        quickSortHelper(pairs, swap + 1, r, r);          // sort right side
+    }
+}
