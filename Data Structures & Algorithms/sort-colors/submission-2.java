4cfdcfcabb3afb4dbbe5af2ba551685a65a2feab
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-04-24 16:09:17 +1000

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] count = {0, 0, 0};
+
+        for (int color : nums) {
+            count[color]++;
+        }
+
+        int i = 0;
+        for (int j = 0; j < count.length; j++) {
+            for (int k = 0; k < count[j]; k++) {
+                nums[i] = j;
+                i++;
+            }
+        }
+    }
+}
