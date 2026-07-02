6224e49b7a9176994ec2b2925346317f92fe6bcf
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-07-02 11:41:49 +1000

    Add: clone-graph - submission-14

---
+++ Data Structures & Algorithms/clone-graph/submission-14.java
@@ -0,0 +1,58 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Deque<Node> queue = new ArrayDeque<>();
+        Set<Node> visited = new HashSet<>();
+        Map<Node, Node> oldToNew = new HashMap<>();
+
+        oldToNew.put(node, new Node(node.val));
+        queue.addLast(node);
+        // visited.add(node);
+
+        // using bfs
+        while (!queue.isEmpty()) {
+            int qLength = queue.size();
+            for (int i = 0; i < qLength; i++) {
+                Node currNode = queue.removeFirst();
+                Node copyNode = oldToNew.get(currNode);
+            
+                // add neighbors to copyNode
+                for (Node neighbor : currNode.neighbors) {
+                    if (oldToNew.containsKey(neighbor)) {
+                        copyNode.neighbors.add(oldToNew.get(neighbor));
+                    }
+                    else {
+                        Node copyNeighbor = new Node(neighbor.val);
+                        oldToNew.put(neighbor, copyNeighbor);
+                        copyNode.neighbors.add(copyNeighbor);
+                        queue.addLast(neighbor);
+                    }
+                }
+            }
+            
+        }
+        return oldToNew.get(node);
+    }
+}
