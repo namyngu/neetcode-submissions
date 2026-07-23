f9c6955f83e3dc277201b746fad7b30afe58e945
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-07-23 11:35:20 +1000

    Add: longest-common-subsequence - submission-5

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-5.java
@@ -0,0 +1,29 @@
+public class Solution {
+    private int[][] memo;
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        memo = new int[text1.length()][text2.length()];
+        for (int i = 0; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return dfs(text1, text2, 0, 0);
+    }
+
+    private int dfs(String text1, String text2, int i, int j) {
+        if (i == text1.length() || j == text2.length()) {
+            return 0;
+        }
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        if (text1.charAt(i) == text2.charAt(j)) {
+            memo[i][j] = 1 + dfs(text1, text2, i + 1, j + 1);
+        } else {
+            memo[i][j] = Math.max(dfs(text1, text2, i + 1, j),
+                                  dfs(text1, text2, i, j + 1));
+        }
+        return memo[i][j];
+    }
+}
