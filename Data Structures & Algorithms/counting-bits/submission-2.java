ea8c1b83df7f38dd1a0b3ab25a18c3ce30a44a14
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-07-24 12:21:55 +1000

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+
+        for (int i = 0; i < res.length; i++) {
+            res[i] = (i & 1) + res[i >> 1];
+        }
+        return res;
+    }
+}
