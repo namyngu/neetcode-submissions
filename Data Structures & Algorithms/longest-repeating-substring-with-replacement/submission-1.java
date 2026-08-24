400352601559d666bd3554ab8932d1a092ea4906
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-08-24 15:50:32 +1000

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,30 @@
+// Intuition: Keep track of frequency of characters as you create a sliding window and move across the array.
+// A substring/window is valid if: substring length - maxFreq <= k
+// If substring is invalid, increment left ptr and update the character frequency (also update max freq.).
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character,Integer> charFreq = new HashMap<>();
+        int res = 0;
+        int l = 0;
+        int maxFreq = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            charFreq.put(s.charAt(r), charFreq.getOrDefault(s.charAt(r), 0) + 1);
+            maxFreq = Math.max(maxFreq, charFreq.get(s.charAt(r)));
+
+            // Check if window is valid.
+            while (r - l + 1 - maxFreq > k) {
+                charFreq.put(s.charAt(l), charFreq.getOrDefault(s.charAt(l), 0) - 1);
+                // update max freq.
+                maxFreq = 0;
+                for (Integer freq : charFreq.values()) {
+                    maxFreq = Math.max(maxFreq, freq);
+                }
+                l++;
+            }
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
