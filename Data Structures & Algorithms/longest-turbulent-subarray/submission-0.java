f9b9e3cd3794e576d61ab82a63a78ba6d173960f
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-08-25 15:00:14 +1000

    Add: longest-turbulent-subarray - submission-0

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-0.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        int res = 1;
+        int l = 0;
+        char signFlag = '=';
+
+        // Edge case
+        if (arr.length == 1) {
+            return 1;
+        }
+
+        for (int r = 1; r < arr.length; r++) {
+            // if (r == 1) {
+            //     int diff = arr[1] - arr[0];
+            //     if (diff > 0) {
+            //         signFlag = '<';
+            //         res = Math.max(res, r - l + 1);
+            //     }
+            //     else if (diff < 0) {
+            //         signFlag = '>';
+            //         res = Math.max(res, r - l + 1);
+            //     }
+            //     else {
+            //         signFlag = '=';
+            //         l = r;
+            //     }
+            // }
+
+            int diff = arr[r] - arr[r - 1];
+            
+            if(diff == 0) {
+                signFlag = '=';
+                l = r;
+            }
+            // still turbulent
+            else if ((diff > 0 && signFlag == '>') || (diff < 0 && signFlag == '<') || signFlag == '=') {
+                signFlag = (diff > 0) ? '<' : '>';
+                res = Math.max(res, r - l + 1);
+            }
+            // not turbulent
+            else {
+                signFlag = (diff > 0) ? '<' : '>';
+                l = r - 1;
+            }
+        }
+        return res;
+    }
+}
