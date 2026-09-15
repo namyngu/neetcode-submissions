9d9822757d6df2e30d8efb19cbfac42c3f1910ed
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-09-15 11:23:41 +1000

    Add: maximum-twin-sum-of-a-linked-list - submission-3

---
+++ Data Structures & Algorithms/maximum-twin-sum-of-a-linked-list/submission-3.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ /* Optimal solution
+    Key Insight: Modify the linked list so that we reverse the ptr at the midpoint so that we can travel back.
+    We reverse the pointers on the first half of the nodes as it is easier to code up.
+    Find midpoint using fast and slow ptrs.
+    
+    Time: O(n)
+    Space: O(1)
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            ListNode tmp = slow;
+            slow = slow.next;
+            tmp.next = prev;
+            prev = tmp;
+        }
+
+        int res = 0;
+        while (slow != null) {
+            res = Math.max(res, prev.val + slow.val);
+            prev = prev.next;
+            slow = slow.next;
+        }
+        return res;
+    }
+}
