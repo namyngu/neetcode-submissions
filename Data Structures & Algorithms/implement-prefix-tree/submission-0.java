f4d44d6142576728af87986e630a4119557f3af8
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-09-16 19:38:28 +1000

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,53 @@
+class PrefixTree {
+    TrieNode root;
+
+    public PrefixTree() {
+         root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+        }
+        curr.word = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return curr.word;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return true;
+    }
+}
+
+public class TrieNode {
+    public Map<Character, TrieNode> children;
+    public boolean word;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        word = false;
+    }
+}
