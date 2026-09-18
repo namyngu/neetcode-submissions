860d35de7072e8aa5b3c670606bc9355ecd24ed4
Nam Nguyen <78007213+namyngu@users.noreply.github.com>
2026-09-18 13:42:59 +1000

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.java
@@ -0,0 +1,62 @@
+class WordDictionary {
+    TrieNode root;
+
+    public WordDictionary() {
+        this.root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+        }
+        curr.isWord = true;
+    }
+
+    public boolean search(String word) {
+        return searchHelper(word, root);
+    }
+
+    public boolean searchHelper(String word, TrieNode root) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == '.') {
+
+                if (i == word.length() - 1) {
+                    for (Map.Entry<Character, TrieNode> entry : curr.children.entrySet()) {
+                        if (entry.getValue().isWord){
+                            return true;
+                        }
+                    }
+                }
+                for (Map.Entry<Character, TrieNode> entry : curr.children.entrySet()) {
+                    if (searchHelper(word.substring(i + 1), entry.getValue())) {
+                        return true;
+                    }
+                }
+                
+                return false;
+            }
+            if (!curr.children.containsKey(c)) {
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return curr.isWord;
+    }
+}
+
+class TrieNode {
+    public Map<Character, TrieNode> children;
+    public boolean isWord;
+
+    public TrieNode() {
+        this.children = new HashMap<>();
+        this.isWord = false;
+    }
+}
